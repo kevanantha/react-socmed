11a25d6d1d50dc978962acffb1a16fa79d1ddd98
Kevin Anantha <kevin.anantha@gmail.com>
2019-05-06 00:05:57 +0800

    Updates

---
+++ precache-manifest.4b59da67c49aecab331525a3b51ad74c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a67c0788307bc69fb1f6",
+    "url": "/qumparantest/static/js/runtime~main.a67c0788.js"
+  },
+  {
+    "revision": "b26aecf9648e21f08a19",
+    "url": "/qumparantest/static/js/main.b26aecf9.chunk.js"
+  },
+  {
+    "revision": "532220ba6ee583dccb2d",
+    "url": "/qumparantest/static/js/1.532220ba.chunk.js"
+  },
+  {
+    "revision": "842587c0dfe25833d082e69340832e61",
+    "url": "/qumparantest/index.html"
+  }
+];
